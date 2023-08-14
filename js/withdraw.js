0bbf00cdec52cb0660c782ce2de3eaf1d8649d9e
azizul96 <alifalamin42@gmail.com>
2023-08-15 02:19:55 +0600

    validation added

--- js/withdraw.js
+++ js/withdraw.js
@@ -4,21 +4,33 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
     const newWithdraw = withdrawField.value;
     const newWithdrawNumber = parseFloat(newWithdraw);
 
+
     const previousWithdrawBox = document.getElementById('previous-withdraw');
     const previousWithdraw = previousWithdrawBox.innerText;
     const previousWithdrawNumber = parseFloat(previousWithdraw);
-
-    const totalWithdraw = previousWithdrawNumber + newWithdrawNumber;
-    previousWithdrawBox.innerText = totalWithdraw;
-
 
     const totalBalanceBox = document.getElementById('total-balance');
     const totalBalance = totalBalanceBox.innerText;
     const totalBalanceNumber = parseFloat(totalBalance);
 
+    withdrawField.value = "";
+
+    if(newWithdrawNumber > totalBalanceNumber){
+        alert('Not Enough Money In Your Baap-Er-Bank' )
+        return;
+    }
+
+    const totalWithdraw = previousWithdrawNumber + newWithdrawNumber;
+    previousWithdrawBox.innerText = totalWithdraw;
+
+    // (ei code guloke if er upore nie gechi validation er jonno)
+    // const totalBalanceBox = document.getElementById('total-balance');
+    // const totalBalance = totalBalanceBox.innerText;
+    // const totalBalanceNumber = parseFloat(totalBalance);
+
     const currentTotalBalance = totalBalanceNumber - newWithdrawNumber;
     totalBalanceBox.innerText = currentTotalBalance;
 
-    withdrawField.value = "";
+    // withdrawField.value = "";
 
 })
