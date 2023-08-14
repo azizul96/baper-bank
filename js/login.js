c8b21ca39c8dc09497948db45612858a75d4042a
azizul96 <alifalamin42@gmail.com>
2023-08-14 22:48:26 +0600

    login added

---
+++ js/login.js
@@ -0,0 +1,12 @@
+document.getElementById('btn-submit').addEventListener('click', function(){
+
+    const emailField = document.getElementById('email-field');
+    const yourEmail = emailField.value;
+
+    const passwordField = document.getElementById('password-field');
+    const yourPassword = passwordField.value;
+
+    if(yourEmail === 'alif@gmail.com' && yourPassword === '1856923505'){
+        window.location.href = "http://127.0.0.1:5500/bank.html"
+    }
+})
